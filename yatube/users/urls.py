03ab99cb247aa21e38b6ac6839c7ee624f635be2
Михаил В. Белоцерковец <m.belotserkovets@smc.local>
2022-09-05 17:24:44 +0300

    pass

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,4 +1,11 @@
-from django.contrib.auth.views import LogoutView, LoginView, PasswordChangeView
+from django.contrib.auth.views import (LogoutView,
+                                       LoginView,
+                                       PasswordChangeView,
+                                       PasswordChangeDoneView,
+                                       PasswordResetView,
+                                       PasswordResetDoneView,
+                                       PasswordResetConfirmView,
+                                       PasswordResetCompleteView, )
 from django.urls import path
 from . import views
 
@@ -25,4 +32,31 @@ urlpatterns = [
         ),
         name='password_change'
     ),
+    path('password_change/done/',
+         PasswordChangeDoneView.as_view(
+             template_name='users/password_change_done.html'
+         ),
+         name='password_change_done'),
+    path('password/reset',
+         PasswordResetView.as_view(
+             template_name='users/password_reset_form.html'
+         ),
+         name='password_reset'),
+    path('password_reset/done/',
+         PasswordResetDoneView.as_view(
+             template_name='users/password_reset_done.html'
+         ),
+         name='passwod_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(
+             template_name='users/password_reset_confirm.html'
+         ),
+         name='pass_confirm'
+         ),
+    path('password_reset_complete',
+         PasswordResetCompleteView.as_view(
+             template_name='users/password_reset_complete.html'
+         ),
+         name='pass_complete'
+         )
 ]
