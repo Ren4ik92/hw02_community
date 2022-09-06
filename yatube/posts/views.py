1cf8b0c139a74b1462f80e0f60ee2cad62a4d11e
Belotserkovets Mikhail <ren4ik92@gmail.com>
2022-09-06 22:51:57 +0300

    page

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
+from django.core.paginator import Paginator
 
 MESSAGE_N = 10
 
 
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.order_by('-pub_date')[:MESSAGE_N]
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, MESSAGE_N)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, template, context)
 
