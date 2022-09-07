4a565100bf6c282ef2ba5809b011307924af13cf
Belotserkovets Mikhail <ren4ik92@gmail.com>
2022-09-07 22:44:27 +0300

    profile

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 MESSAGE_N = 10
 
@@ -29,12 +30,22 @@ def group_posts(request, slug):
 
 
 def profile(request, username):
+    author = get_object_or_404(author__username=username)
+    user_post_lost = Post.objects.filter(author__username=username)
+    paginator = Paginator(user_post_lost, MESSAGE_N)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
+        'page_obj': page_obj
     }
     return render(request, 'posts/profile.html', context)
 
 
 def post_detail(request, post_id):
+    post_d = group_posts(Post, pk=post_id)
+
     context = {
+        'post_d': post_d
     }
     return render(request, 'posts/post_detail.html', context)
